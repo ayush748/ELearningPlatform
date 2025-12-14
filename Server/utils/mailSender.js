439218c32e2e0634aa1d329fbdaadf589a9e8ede
Ayush Tiwari <iamayush522@gmail.com>
2025-12-14 21:04:21 +0530

    File restructured and Frontend Homepage added

---
+++ Server/utils/mailSender.js
@@ -0,0 +1,57 @@
+// const nodemailer = require("nodemailer");
+
+// const mailSender = async (email, title, body) => {
+//     try { 
+//         let transporter = nodemailer.createTransport({
+//             host : process.env.MAIL_HOST,
+//             auth : {
+//                 user : process.env.MAIL_USER,
+//                 pass: process.env.MAIL_PASS,
+//             },
+//         });
+
+//         let info = await transporter.sendMail({
+//             from: 'StudyNotion || CodeHelp ',
+//             to: `${email}`,
+//             subject: `${title}`,
+//             html: `${body}`,
+//         })
+//         console.log(info);
+//         return info;
+
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
+
+// module.exports = mailSender;
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title, body) => {
+    try {
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        let info = await transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: email,
+            subject: title,
+            html: body,
+        });
+
+        return info;
+
+    } catch (error) {
+        console.log("Mail error:", error);
+        throw error;
+    }
+};
+
+module.exports = mailSender;
